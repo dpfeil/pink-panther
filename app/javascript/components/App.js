5e9e216286f14b685032bd4de3fa4b7829b3aa60
Doug Pfeil <dpfeil@gmail.com>
2022-02-03 11:00:22 -0500

    Update to use Screen component

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,36 +1,12 @@
 import React from "react"
-import Pixel from './Pixel'
+import Screen from './Screen'
 
 import './app.scss'
 
-const HEIGHT = 20
-const WIDTH = 40
-
-function getRandomInt(max) {
-  return Math.floor(Math.random() * max);
-}
-
-
-const generateRandomColor = () => {
-  return `rgb(${getRandomInt(256)},${getRandomInt(256)},${getRandomInt(256)})`
-}
-const App = () => {
-  const pixels = []
-  for (let y = 0; y < HEIGHT; ++y) {
-
-    const rows = []
-    for (let x = 0; x < WIDTH; ++x) {
-      rows.push(<Pixel key={x} color={generateRandomColor()} />)
-    }
-    pixels.push(<div key={y} className="pixel-row">{rows}</div>)
-  }
-
-  return (
-    <div>
-      {pixels}
-    </div>
-  )
-
-}
+const App = () => (
+  <div>
+    <Screen />
+  </div>
+)
 
 export default App
