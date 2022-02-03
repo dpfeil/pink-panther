128cf70efbea9b039313db24d733f8740cf4ee57
Doug Pfeil <dpfeil@gmail.com>
2022-02-03 10:27:41 -0500

    Add display of pixel matrix to App component

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,7 +1,36 @@
 import React from "react"
+import Pixel from './Pixel'
 
-const App = () => (
-  <div>This is the app</div>
-)
+import './app.scss'
+
+const HEIGHT = 20
+const WIDTH = 40
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
+
+const generateRandomColor = () => {
+  return `rgb(${getRandomInt(256)},${getRandomInt(256)},${getRandomInt(256)})`
+}
+const App = () => {
+  const pixels = []
+  for (let y = 0; y < HEIGHT; ++y) {
+
+    const rows = []
+    for (let x = 0; x < WIDTH; ++x) {
+      rows.push(<Pixel key={x} color={generateRandomColor()} />)
+    }
+    pixels.push(<div key={y} className="pixel-row">{rows}</div>)
+  }
+
+  return (
+    <div>
+      {pixels}
+    </div>
+  )
+
+}
 
 export default App
